b718c878fb2eb4aeb82fbf097d9684156e24ee5c
tryharder <maiorog.prog@gmail.com>
2015-06-21 23:15:01 +0300

    implemented percolation java

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,9 +1,132 @@
 public class Percolation {
-    public Percolation(int N) {
 
-    }              // create N-by-N grid, with all sites blocked
-    public void open(int i, int j)          // open site (row i, column j) if it is not open already
-    public boolean isOpen(int i, int j)     // is site (row i, column j) open?
-    public boolean isFull(int i, int j)     // is site (row i, column j) full?
-    public boolean percolates()             // does the system percolate?
+    private final int VIRTUAL_TOP_INDEX = 0;
+    private final int VIRTUAL_BOTTOM_INDEX = 1;
+
+    private boolean isOpened[];
+    private WeightedQuickUnionUF quickUnion;
+    private int size;
+
+    public Percolation(int N) {
+        this.quickUnion = new WeightedQuickUnionUF(N * N + 2);
+        this.size = N;
+        isOpened = new boolean[N * N + 2];
+
+        for (int i = 2; i < N + 2; i++) {
+            quickUnion.union(VIRTUAL_TOP_INDEX, i);
+        }
+
+        for (int i = getIndexFromMatrixCoords(N, 0) - size; i < getIndexFromMatrixCoords(N, 0); i++) {
+            quickUnion.union(VIRTUAL_BOTTOM_INDEX, i);
+        }
+
+    }
+
+    public void open(int i, int j) {
+        int index = getIndexFromMatrixCoords(i, j);
+
+        if (i == 0 && j == 0) {
+            int index_bottom = getIndexFromMatrixCoords(i + 1, j);
+            int index_right = getIndexFromMatrixCoords(i, j + 1);
+            if (isOpen(i + 1, j))
+                quickUnion.union(index, index_bottom);
+            if (isOpen(i, j + 1))
+                quickUnion.union(index, index_right);
+        } else if (i == 0 && j == size - 1) {
+            int index_left = getIndexFromMatrixCoords(i, j - 1);
+            int index_bottom = getIndexFromMatrixCoords(i + 1, j);
+            if (isOpen(i, j - 1))
+                quickUnion.union(index, index_bottom);
+            if (isOpen(i + 1, j))
+                quickUnion.union(index, index_left);
+        } else if (i == 0) {
+            int index_left = getIndexFromMatrixCoords(i, j - 1);
+            int index_bottom = getIndexFromMatrixCoords(i + 1, j);
+            int index_right = getIndexFromMatrixCoords(i, j + 1);
+            if (isOpen(i,j - 1))
+                quickUnion.union(index, index_bottom);
+            if (isOpen(i+1, j))
+                quickUnion.union(index, index_left);
+            if (isOpen(i, j + 1))
+                quickUnion.union(index, index_right);
+        } else if (i == size - 1 && j == 0) {
+            int index_top = getIndexFromMatrixCoords(i - 1, j);
+            int index_right = getIndexFromMatrixCoords(i, j + 1);
+            if (isOpen(i - 1, j))
+                quickUnion.union(index, index_top);
+            if (isOpen(i, j + 1))
+                quickUnion.union(index, index_right);
+        } else if (i == size - 1 && j == size - 1) {
+            int index_top = getIndexFromMatrixCoords(i - 1, j);
+            int index_left = getIndexFromMatrixCoords(i, j - 1);
+            if (isOpen(i-1,j))
+                quickUnion.union(index, index_top);
+            if (isOpen(i, j - 1))
+                quickUnion.union(index, index_left);
+        } else if (i == size - 1) {
+            int index_top = getIndexFromMatrixCoords(i - 1, j);
+            int index_left = getIndexFromMatrixCoords(i, j - 1);
+            int index_right = getIndexFromMatrixCoords(i, j + 1);
+            if (isOpen(i - 1, j))
+                quickUnion.union(index, index_top);
+            if (isOpen(i, j - 1))
+                quickUnion.union(index, index_left);
+            if (isOpen(i, j + 1))
+                quickUnion.union(index, index_right);
+        } else if (j == 0 && i > 0) {
+            int index_top = getIndexFromMatrixCoords(i - 1, j);
+            int index_bottom = getIndexFromMatrixCoords(i + 1, j);
+            int index_right = getIndexFromMatrixCoords(i, j + 1);
+            if (isOpen(i - 1, j))
+                quickUnion.union(index, index_top);
+            if (isOpen(i+1, j))
+                quickUnion.union(index, index_bottom);
+            if (isOpen(i, j + 1))
+            quickUnion.union(index, index_right);
+        } else if (i > 0 && j == size - 1) {
+            int index_top = getIndexFromMatrixCoords(i - 1, j);
+            int index_left = getIndexFromMatrixCoords(i, j - 1);
+            int index_bottom = getIndexFromMatrixCoords(i + 1, j);
+            if (isOpen(i - 1, j))
+                quickUnion.union(index, index_top);
+            if (isOpen(i, j - 1))
+                quickUnion.union(index, index_left);
+            if (isOpen(i + 1, j))
+                quickUnion.union(index, index_bottom);
+        } else if (i > 0 && j > 0) {
+            int index_top = getIndexFromMatrixCoords(i - 1, j);
+            int index_left = getIndexFromMatrixCoords(i, j - 1);
+            int index_bottom = getIndexFromMatrixCoords(i + 1, j);
+            int index_right = getIndexFromMatrixCoords(i, j + 1);
+            if (isOpen(i - 1, j))
+                quickUnion.union(index, index_top);
+            if (isOpen(i, j - 1))
+                quickUnion.union(index, index_bottom);
+            if (isOpen(i+1, j))
+                quickUnion.union(index, index_left);
+            if (isOpen(i, j + 1))
+                quickUnion.union(index, index_right);
+        }
+
+        isOpened[index] = true;
+
+    }
+
+    public boolean isOpen(int i, int j) {
+        int index = getIndexFromMatrixCoords(i, j);
+        return isOpened[index];
+    }
+
+    public boolean isFull(int i, int j) {
+        int index = getIndexFromMatrixCoords(i, j);
+        return isOpen(i, j) && quickUnion.connected(VIRTUAL_TOP_INDEX, index);
+    }
+
+    public boolean percolates() {
+        return quickUnion.connected(VIRTUAL_TOP_INDEX, VIRTUAL_BOTTOM_INDEX);
+    }
+
+    private int getIndexFromMatrixCoords(int i, int j) {
+        return size * i + j + 2;
+    }
 }
--- src/test/java/PercolationTest.java
+++ src/test/java/PercolationTest.java
@@ -1,3 +1,74 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class PercolationTest {
 
+    private Percolation percolation;
+    private int size = 3;
+
+    @Before
+    public void setUp() {
+        percolation = new Percolation(size);
+    }
+
+    @Test
+    public void anyCellIsCloseAtTheBeginning() {
+        assertThat(percolation.isOpen(1, 1), is(false));
+    }
+
+    @Test
+    public void anyCellIsOpenAfterClientOpenedIt() {
+        percolation.open(1,1);
+
+        assertThat(percolation.isOpen(1,1), is(true));
+    }
+
+    @Test
+    public void anyTopRowCellIsFullWhenItIsOpened() {
+        for(int i = 2; i < size; i++) {
+            percolation.open(0, i);
+            assertThat(percolation.isFull(0, i), is(true));
+        }
+
+
+    }
+
+    @Test
+    public void percolatesWhenTopAnyTopCellConnectedToAnyBottomCell() {
+        percolation.open(0,0);
+        percolation.open(1,0);
+        percolation.open(2,0);
+
+        assertThat(percolation.percolates(), is(true));
+    }
+
+    @Test
+    public void percolatestWhenAConnectionExists() {
+        Percolation percolation = new Percolation(5);
+
+        percolation.open(0,3);
+        percolation.open(1,3);
+        percolation.open(2,3);
+        percolation.open(3,3);
+        percolation.open(3,4);
+        percolation.open(4,4);
+
+        assertThat(percolation.percolates(), is(true));
+    }
+
+    @Test
+    public void notPercolateWhenThereIsNoConnectionBetweenTopAndBot() {
+        Percolation percolation = new Percolation(5);
+
+        percolation.open(0,3);
+        percolation.open(1,3);
+        percolation.open(2,3);
+        percolation.open(3,3);
+        percolation.open(3,4);
+
+        assertThat(percolation.percolates(), is(false));
+    }
 }
