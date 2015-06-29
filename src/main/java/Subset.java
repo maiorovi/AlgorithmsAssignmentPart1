c4b623730f0a249d89222dc85c63e4d65559a2f5
tryharder <maiorog.prog@gmail.com>
2015-06-30 00:03:39 +0300

    * added subset class

---
+++ src/main/java/Subset.java
@@ -0,0 +1,17 @@
+public class Subset {
+
+    public static void main(String[] args) {
+
+        int k = Integer.parseInt(args[0]);
+
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+
+        while(!StdIn.isEmpty()) {
+            queue.enqueue(StdIn.readString());
+        }
+
+        for (int i = 0; i < k; i++) {
+            System.out.println(queue.dequeue());
+        }
+    }
+}
