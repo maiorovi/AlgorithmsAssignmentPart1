e893e6836b1f4432a81f9e4cfe880b291284ab12
tryharder <maiorog.prog@gmail.com>
2015-08-03 23:19:32 +0300

    * made board immutable

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -2,13 +2,18 @@ import java.util.ArrayList;
 
 public class Board {
 
-    private final int[][] board;
+    private int[][] board;
 
     public Board(int board[][]) {
         if (board.length != board[0].length)
             throw new IllegalArgumentException();
+        this.board = new int[board.length][board.length];
 
-        this.board = board;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                this.board[i][j] = board[i][j];
+            }
+        }
     }
 
     public int dimension() {
